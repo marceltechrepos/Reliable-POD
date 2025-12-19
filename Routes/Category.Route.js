a1ebab6a10c237c95a49c9c075c1d4b049abd2ea
Farzam <89484118+FarzamN@users.noreply.github.com>
2025-12-19 21:02:35 +0500

    update product api updated

--- Routes/Category.Route.js
+++ Routes/Category.Route.js
@@ -1,13 +1,35 @@
 import { Router } from "express";
 import { upload } from "../Middlewares/Multer/multer.js";
-import { createCategory, createProvider, getAllCategory, getProviders } from "../Controllers/Category.Controller.js";
+import {
+  createCategory,
+  createProvider,
+  getAllCategory,
+  getProviders,
+} from "../Controllers/Category.Controller.js";
 import { isAdmin } from "../Middlewares/Authentication/Auth.middleware.js";
 
 const CategoryRouter = Router();
 
-CategoryRouter.post("/Category/create-category", isAdmin, upload.single("thumbnail"), createCategory)
-CategoryRouter.get("/Category/get-all-category", isAdmin, getAllCategory)
+CategoryRouter.post(
+  "/Category/create-category",
+  //  isAdmin,
+  upload.single("thumbnail"),
+  createCategory
+);
+CategoryRouter.get(
+  "/Category/get-all-category",
+  //  isAdmin,
+  getAllCategory
+);
 
-CategoryRouter.post("/Provider/create-provider", isAdmin, createProvider);
-CategoryRouter.get("/Provider/get-all-provider", isAdmin, getProviders);
+CategoryRouter.post(
+  "/Provider/create-provider",
+  //  isAdmin,
+  createProvider
+);
+CategoryRouter.get(
+  "/Provider/get-all-provider",
+  //  isAdmin,
+  getProviders
+);
 export default CategoryRouter;
