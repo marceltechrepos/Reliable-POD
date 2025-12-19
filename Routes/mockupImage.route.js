5b2da77b169fba14ca3142756573c8b78a87a1f1
001hamzaimran <syed.hamza2004@gmail.com>
2025-12-19 16:34:48 +0500

    minor changes

--- Routes/mockupImage.route.js
+++ Routes/mockupImage.route.js
@@ -4,11 +4,12 @@ import {
   getAllMockupImage,
   uploadMockupImage,
 } from "../Controllers/mockupImage.controller.js";
+import { upload } from "../Middlewares/Multer/multer.js";
 
 const router = Router();
 
 router.get("/get-mockup-image", getAllMockupImage);
-router.post("/upload-mockup-image", uploadMockupImage);
+router.post("/upload-mockup-image", upload.single("thumbnail"), uploadMockupImage);
 router.delete("/delete-mockup-image/:id", deleteMockupImage);
 
 export default router;
