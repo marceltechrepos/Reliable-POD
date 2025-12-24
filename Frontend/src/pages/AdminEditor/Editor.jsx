020e6b3c4cdab5f22d231029c553c0d849819922
Noman229 <nkdeveloper229@gmail.com>
2025-12-24 15:14:05 +0500

    Working selected mockup

--- Frontend/src/pages/AdminEditor/Editor.jsx
+++ Frontend/src/pages/AdminEditor/Editor.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Editor() {
+    const savedMockup = localStorage.getItem('mockupToEdit');
+    console.log(savedMockup)
     return (
         <div className='w-screen h-screen bg-gray-900 text-white'>Editor</div>
     )
