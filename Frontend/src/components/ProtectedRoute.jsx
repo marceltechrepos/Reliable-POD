892e7ce699f72ba5cdcf866ea34d610d1bfcadff
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2026-01-02 20:41:47 +0500

    comment code remove

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -1,20 +1,3 @@
-// import { Navigate, Outlet } from "react-router-dom";
-
-// const ProtectedRoute = () => {
-//   const token = localStorage.getItem("token");
-
-//   // agar token nahi → login
-//   if (!token) {
-//     return <Navigate to="/login" replace />;
-//   }
-
-//   // token hai → andar jane do
-//   return <Outlet />;
-// };
-
-// export default ProtectedRoute;
-
-
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = ({ adminOnly = false }) => {
