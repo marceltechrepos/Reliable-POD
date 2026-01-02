9063c51aa73d9d5936dccc6b5ca425cfd0ed440f
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2026-01-02 20:41:47 +0500

    comment code remove

--- Frontend/layout/Admin/AdminLayout.jsx
+++ Frontend/layout/Admin/AdminLayout.jsx
@@ -1,37 +1,3 @@
-// import { Outlet } from "react-router-dom";
-// import Sidebar from "../../src/components/Sidebar";
-// import Topbar from "../../src/components/Topbar";
-// import { useState } from "react";
-
-// const AdminLayout = () => {
-//     const [sidebarOpen, setSidebarOpen] = useState(false);
-
-//     return (
-//         <>
-//             {/* Admin Header / Sidebar */}
-
-
-//             <Topbar onMenuClick={() => setSidebarOpen(true)} />
-//             <div className='flex bg-slate-100'>
-//                 <Sidebar
-//                     isOpen={sidebarOpen}
-//                     onClose={() => setSidebarOpen(false)}
-//                 />
-//                 <main className="flex-1 w-5/6 bg-gray-50">
-//                     <Outlet />
-//                 </main>
-//             </div>
-
-
-
-//         </>
-//     );
-// };
-
-// export default AdminLayout;
-
-
-
 import { Outlet } from "react-router-dom";
 import Sidebar from "../../src/components/Sidebar";
 import Topbar from "../../src/components/Topbar";
