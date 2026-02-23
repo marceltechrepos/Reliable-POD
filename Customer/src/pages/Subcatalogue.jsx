70aa83b06c63549b700c9485a91a8effa5a9ba4f
Minhaj0316 <muhammadminhaj@marceltech.com>
2026-02-23 14:00:05 +0500

    ggg

--- Customer/src/pages/Subcatalogue.jsx
+++ Customer/src/pages/Subcatalogue.jsx
@@ -82,7 +82,7 @@ return (
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
           {filtered.map((c) => (
         <Link 
-              to={c.link || `/user/products`}
+              to={c.link || `/user/Product-catloge`}
               key={c.id} 
               className="group relative font-sans w-full block no-underline cursor-pointer"
             >
